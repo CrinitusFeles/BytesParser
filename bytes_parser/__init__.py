1b137f53c411920714782e521bef113f6ee8e966
CrinitusFeles <crinitusfeles@gmail.com>
2026-04-20 17:38:14 +0700

    fixed __init__.py

--- bytes_parser/__init__.py
+++ bytes_parser/__init__.py
@@ -1,5 +1,6 @@
 from .bitfields import BitField, BitFlag  # noqa: F401
 from .frame import Frame  # noqa: F401
 from .row import Row  # noqa: F401
+from .subframe import SubFrame  # noqa: F401
 
 __version__ = '3.0.0'
